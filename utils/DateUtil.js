dd4a3a4a6f1360b50fa94564db511ea197610660
Maid <zhp92@outlook.com>
2022-03-10 22:29:33 +0800

    日期处理工具类

--- utils/DateUtil.js
+++ utils/DateUtil.js
@@ -1,7 +1,27 @@
-const getChinaTime = () => { 
-  return new Date(new Date().getTime() + 8 * 60 * 60 * 1000)
+const getChinaTime = (date = new Date()) => { 
+  return new Date(date.getTime() + 8 * 60 * 60 * 1000)
+}
+
+const getDate = (date = new Date()) => {
+  return getChinaTime(new Date(getDateStr(date)));
+}
+
+const getWeekAgo = () => {
+  const now = getChinaTime();
+  const lastWeek = now.getTime() - 7 * 24 * 60 * 60 * 1000;
+  return new Date(lastWeek);
+}
+
+const getDateStr = (date)=>{
+  const y = date.getFullYear();
+  const m = date.getMonth() + 1;
+  const d = date.getDate();
+  return y + '-' + m + '-' + d;
 }
 
 module.exports = {
   getChinaTime,
+  getAWeekAgo: getWeekAgo,
+  getDateStr,
+  getDate
 }
