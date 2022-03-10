c105ad767b05294211b8e37c11fa37db55cc20b8
Maid <zhp92@outlook.com>
2022-03-10 23:37:10 +0800

    入口文件主逻辑

---
+++ index.js
@@ -0,0 +1,19 @@
+const { ws, http } = require('./bot')
+const config = require('./config')
+
+const rPlugins = Object.keys(config.plugins.rPlugins).map(name =>
+  require(name)(config.plugins.rPlugins[name] || {})
+)
+const sPlugins = Object.keys(config.plugins.sPlugins).map(name =>
+  require(name)(config.plugins.sPlugins[name] || {})
+)
+
+ws.listen(data => {
+  if (process.env.NODE_ENV === 'development') {
+    data.meta_event_type !== 'heartbeat' && console.log(data)
+  }
+
+  rPlugins.forEach(plugin => plugin({ data, ws, http }))
+})
+
+sPlugins.forEach(plugin => plugin({ ws, http }))
