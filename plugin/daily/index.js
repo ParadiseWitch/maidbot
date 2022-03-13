2339dccb6e1a05f4c95d56d42fbcf2abc8eac2e7
Maid <zhp92@outlook.com>
2022-03-13 15:03:20 +0800

    daily temp

--- plugin/daily/index.js
+++ plugin/daily/index.js
@@ -1,18 +1,12 @@
 const { addDaily, getWeekDaily } = require("./service/dailyService");
 
-module.exports = opts => { 
-  return async ({data, ws, http}) => {
-    if(!data.message) return;
+module.exports = opts => {
+  return async ({ data, ws, http }) => {
+    if (!data.message) return;
     if (data.message_type !== 'private') return;
-    if(data.message.indexOf('打卡') === -1) return;
+    if (data.message.indexOf('打卡') === -1) return;
     const SENDER_ID = 1374624878;
     if (data.sender.user_id !== SENDER_ID) return;
-    if(data.message === '打卡'){
-      await addDaily();
-      ws.send('send_private_msg',{
-        user_id: data.sender.user_id,
-        message: '打卡成功\n' + await getWeekDaily()
-      })
-    }
+
   }
 }
