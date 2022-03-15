caf9da3a96e5d51dff8b01b3ea5831897a13c3ff
Maid <zhp92@outlook.com>
2022-03-15 10:47:04 +0800

    daily主逻辑对接

--- plugin/daily/index.js
+++ plugin/daily/index.js
@@ -1,4 +1,5 @@
 const { addDaily, getWeekDaily } = require("./service/dailyService");
+const dailyHandlers = require("./handler.js");
 
 module.exports = opts => {
   return async ({ data, ws, http }) => {
@@ -7,6 +8,11 @@ module.exports = opts => {
     if (data.message.indexOf('打卡') === -1) return;
     const SENDER_ID = 1374624878;
     if (data.sender.user_id !== SENDER_ID) return;
-
+    const handlers = dailyHandlers.filter(handler => handler.match(data));
+    if (handlers.length <= 0) { 
+      await dailyHandlers[0].handle({ data, ws, http });
+      return;
+    }
+    handlers.forEach(handler => handler.handle({ data, ws, http }));
   }
 }
